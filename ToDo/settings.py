96fd87cc358c1d8b03cb928a9f868d253c82eb2c
Ray786719 <maryum112000@yahoo.com>
2025-10-11 22:30:13 +0100

    security: Fix SECRET_KEY exposure - useenvironment variables

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-e^t1che+)p2g2$mpzqq=mf1p^@p=!i($4n+4_n#bzj$p=#c92b')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
